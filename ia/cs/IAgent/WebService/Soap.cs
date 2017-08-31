0842be3c3374727135de70b55160efa55c8b8772
Rustam Shakirov <rushchel@gmail.com>
2017-08-31 18:23:10 +0600

    use automapper

--- ia/cs/IAgent/WebService/Soap.cs
+++ ia/cs/IAgent/WebService/Soap.cs
@@ -7,6 +7,10 @@ using WS.Ekassir;
 //using System.Data.OracleClient;
 using Oracle.DataAccess.Client;
 using Oracle.DataAccess.Types;
+using AutoMapper;
+using AutoMapper.Configuration;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WS
 {
@@ -16,6 +20,70 @@ namespace WS
 	{
 		public const string XmlNS = "http://localhost/ia/";
         public const string oradb = "Data Source=XE;User ID=RUSH;Password=rush";
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+
+        public sealed class SimplePersonProfile : Profile
+        {
+            protected /*override*/ void Configure()
+            {
+                IMappingExpression<DataRow, Person> mappingExpression;
+
+                mappingExpression = CreateMap<DataRow, Person>();
+                mappingExpression.ForMember(d => d.FirstName, o => o.MapFrom(s => s["FirstName"]));
+                mappingExpression.ForMember(d => d.LastName, o => o.MapFrom(s => s["LastName"]));
+                mappingExpression.ForMember(d => d.Age, o => o.MapFrom(s => s["Age"]));
+
+                return;
+            }
+
+            public SimplePersonProfile()
+            {
+                Configure();
+            }
+        }
+
+        [WebMethod(Description = "Person")]
+        public int GetPerson()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("FirstName", typeof(string));
+            dt.Columns.Add("LastName", typeof(string));
+            dt.Columns.Add("Age", typeof(int));
+
+            var dr = dt.NewRow();
+            dr["FirstName"] = "Person";
+            dr["LastName"] = "One";
+            dr["Age"] = 23;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["FirstName"] = "Person";
+            dr["LastName"] = "Two";
+            dr["Age"] = 43;
+            dt.Rows.Add(dr);
+
+            /*Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<IDataReader, List<Person>>();
+                cfg.CreateMap<IDataReader, IEnumerable<Person>>();
+            });
+            List<Person> people = Mapper.Map<IDataReader, List<Person>>(dt.CreateDataReader());*/
+
+            MapperConfiguration configuration;
+            configuration = new MapperConfiguration(a =>
+            {
+                a.AddProfile(new SimplePersonProfile());
+            });
+            IMapper mapper = configuration.CreateMapper();
+            List<DataRow> list = dt.AsEnumerable().ToList();
+            List<Person> people = mapper.Map<List<DataRow>, List<Person>>(list);
+            return people.Count;
+        }
 
         [WebMethod(Description = "Plus")]
 		public int WebAdd(int x, int y)
@@ -26,7 +94,7 @@ namespace WS
 		[WebMethod(Description = "Table request")]
 		public string GetTable()
 		{
-			using (OracleConnection conn = new OracleConnection(oradb))
+            using (OracleConnection conn = new OracleConnection(oradb))
 			{
                 try
                 {
@@ -42,8 +110,7 @@ namespace WS
 
                     DataSet ds = new DataSet();
                     da.Fill(ds);
-                    string xmlDS = ds.GetXml();
-                    return xmlDS;
+                    return ds.Tables["Client"].Rows[0]["Name"].ToString();
                 }
                 catch (Exception ex)
                 {
@@ -52,56 +119,52 @@ namespace WS
 			}
 		}
 
-        [WebMethod(Description = "Objects request")]
-        public string GetObjects()
-        {
-            using (OracleConnection conn = new OracleConnection(oradb))
-            {
-                try
-                {
-                    OracleCommand cmd = new OracleCommand("CURSPKG.GET_OBJECTS", conn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("INP_NUM", OracleDbType.Double, 100).Value = 123;
-                    cmd.Parameters.Add("REC", OracleDbType.Object).Direction = ParameterDirection.Output;
-                    cmd.Parameters.Add("TAB", OracleDbType.Object).Direction = ParameterDirection.Output;
-
-                    OracleDataAdapter da = new OracleDataAdapter(cmd);
-                    da.TableMappings.Add("Table", "Record");
-                    da.TableMappings.Add("Table1", "Table");
-
-                    DataSet ds = new DataSet();
-                    da.Fill(ds);
-                    string xmlDS = ds.GetXml();
-                    return xmlDS;
-                }
-                catch (Exception ex)
-                {
-                    return ex.Message;
-                }
-            }
-        }
-
         [WebMethod(Description = "Dictinary request")]
 		public Response GetDirectory (GetDirectoryRequest request)
 		{
 			GetDirectoryResponse response = new GetDirectoryResponse();
 			Client client = new Client();
 			Requisites requisites = new Requisites();
-			client.Id = 101;
-			//client.Name = "OTPBANK";
-			client.Name = this.Context.Request.Headers.Get("Content-Type");
-			client.Blocked = false;
-			requisites.LegalName = "АО ОТП БАНК";
-			requisites.LegalAddress = "ул. Орджоникидзе 3А";
-			requisites.PostAddress = "г. Омск - 644043, ул. Орджоникидзе 3А";
-			requisites.TaxpayerIdNumber = "550777777777";
-			requisites.RRCode = "550999999999";
-			requisites.ContactPhoneNumber = "+73812123456";
-			client.Requisites = requisites;
-			response.Client = client;
-			this.Context.Response.AppendHeader("eKassir-Version", "666");
-			return response;
-		}
+            using (OracleConnection conn = new OracleConnection(oradb))
+            {
+                try
+                {
+                    OracleCommand cmd = new OracleCommand("CURSPKG.TWO_CURSORS", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("INP_NUM", OracleDbType.Double, 100).Value = 123;
+                    cmd.Parameters.Add("CURSOR_01", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("CURSOR_02", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+
+                    OracleDataAdapter da = new OracleDataAdapter(cmd);
+                    da.TableMappings.Add("Table", "Client");
+                    da.TableMappings.Add("Table1", "ClientRequisites");
+
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    client.Id = Convert.ToInt32(ds.Tables["Client"].Rows[0]["Id"]);
+                    client.Name = ds.Tables["Client"].Rows[0]["Name"].ToString();
+                    client.Blocked = (ds.Tables["Client"].Rows[0]["Blocked"].ToString() == "1") ? true : false;
+
+                    //List<Client> clients = AutoMapper.Mapper.Map<IDataReader, List<Client>>(ds.Tables["Client"].CreateDataReader());
+                    //client.Id = clients[1].Id;
+                    requisites.LegalName = "АО ОТП БАНК";
+			        requisites.LegalAddress = "ул. Орджоникидзе 3А";
+			        requisites.PostAddress = "г. Омск - 644043, ул. Орджоникидзе 3А";
+			        requisites.TaxpayerIdNumber = "550777777777";
+			        requisites.RRCode = "550999999999";
+			        requisites.ContactPhoneNumber = "+73812123456";
+			        client.Requisites = requisites;
+			        response.Client = client;
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    ErrorResponse err = new ErrorResponse();
+                    err.Error.Description = ex.Message;
+                    return err;
+                }
+            }
+        }
 			
 		/// <summary>
 		/// Logs into the web service
